8b57f65db7f11c308cb98bcb38e4d5d326574bc1
Irina Semakova <semakovais@yandex.ru>
2025-11-29 20:53:05 +0300

    9 commit

--- src/components/Main/About.jsx
+++ src/components/Main/About.jsx
@@ -1,18 +1,14 @@
+import React from 'react';
 import "../Main/main.css";
 
 const About = () => {
   return (
     <section className="about" id="about">
-     
-      <h2 className="text-left about-title">О нас</h2>
-      
-      <div className="text-left">
-      <div className="border-left-about"></div>
-        {" "}
+      <h2 className="title-about">О нас</h2>
+      <div className="paragraphs">
         <p className="paragraph">
-          {" "}
-          Мы рады видеть Вас! Мы работаем для Вас с 2003года. 14 лет мы
-          наблюдаем как с каждым днем всё больше людей заказывают жд билеты
+          Мы рады видеть Вас! Мы работаем для Вас с 2003 года. 14 лет мы
+          наблюдаем, как с каждым днем всё больше людей заказывают жд билеты
           через интернет.
         </p>
         <p className="paragraph">
@@ -20,7 +16,7 @@ const About = () => {
           но стоит ли это делать? Мы расскажем о преимуществах заказа через
           интернет.
         </p>
-        <p className="paragraph-bold">
+        <p className="paragraph paragraph-bold">
           Покупать жд билеты дешево можно за 90 суток до отправления поезда.
           Благодаря динамическому ценообразованию цена на билеты в это время
           самая низкая.
