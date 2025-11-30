357e5c6aeed78d217507dca5bb0a7e3bb4d03a5a
Irina Semakova <semakovais@yandex.ru>
2025-11-30 20:53:50 +0300

    10 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Layout from "./components/Layout";
-import HomePage from "./components/Pages/Pages";
-import SelectionTrain from "./components/Pages/Pages";
-import SelectionWagons from "./components/Pages/Pages";
-import PassengersInfo from "./components/Pages/Pages";
-import PersonalData from "./components/Pages/Pages";
-import Screening from "./components/Pages/Pages";
-import OrderResult from "./components/Pages/Pages";
-import NotFound from "./components/Pages/Pages";
-import "./App.css";
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import { 
+  HomePage, 
+  SelectionTrain, 
+  SelectionWagons, 
+  PassengersInfo, 
+  PersonalData, 
+  Screening, 
+  OrderResult, 
+  NotFound 
+} from './components/Pages/Pages'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/fe-diplom/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="trains/" element={<SelectionTrain />} />
-          <Route path="seats/:id" element={<SelectionWagons />} />
-          <Route path="passengers/:id/" element={<PassengersInfo />} />
-          <Route path="personal_information/:id/" element={<PersonalData />} />
-          <Route path="screening/:id/" element={<Screening />} />
-          <Route path="order-result/:id/" element={<OrderResult/>} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      
-      </Routes>
-    </BrowserRouter>
-  );
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="trains" element={<SelectionTrain />} />
+        <Route path="seats/:id" element={<SelectionWagons />} />
+        <Route path="passengers/:id" element={<PassengersInfo />} />
+        <Route path="personal_information/:id" element={<PersonalData />} />
+        <Route path="screening/:id" element={<Screening />} />
+        <Route path="order-result/:id" element={<OrderResult />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  )
 }
 
-export default App;
+export default App
--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import  store  from '../src/store/store';
-import App from './App.jsx';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import  store from './store/store.js'
+import App from './App.jsx'
+import './index.css'
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-//console.log(store.getState())
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <Provider store={store}>
-      <App />
-      </Provider>
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Provider store={store}>
+      <BrowserRouter basename="/fe-diplom">
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+)
