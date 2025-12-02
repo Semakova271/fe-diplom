d93c6a41deb3eb236ec673e01ee5ea35db9a23e3
Irina Semakova <semakovais@yandex.ru>
2025-12-02 18:05:28 +0300

    10 commit

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/fe-diplom/',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+        external: [],
+    },
+  },
+})
